b0ea9e4b5fa34376a4f70bf06e761287550330f3
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-24 14:24:58 +0500

    added project

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,7 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        if (amount==0) {return boundary;};
         if ((amount % boundary) == 0) {
             return 0;
         } else {
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
+    void shouldRemainAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void shouldRemain0AmountIsMultiple1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
