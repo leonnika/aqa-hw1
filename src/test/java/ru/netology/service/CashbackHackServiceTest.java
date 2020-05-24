f44d8ac7106d2ea5256f2928f8e7001994755f43
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-24 14:55:43 +0500

    changed fot TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.assertEquals;//для TestNG
 public class CashbackHackServiceTest {
 
     @Test
-    void shouldRemainAmountIs0() {
+   public void shouldRemainAmountIs0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
@@ -16,7 +16,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    void shouldRemain0AmountIsMultiple1000() {
+   public void shouldRemain0AmountIsMultiple1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
@@ -25,7 +25,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    void shouldRemainAmountNotMultiple1000() {
+   public void shouldRemainAmountNotMultiple1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
