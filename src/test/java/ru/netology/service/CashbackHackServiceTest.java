ed7e2bdf90572580914bdd89ee52f8d8a6dc0470
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-24 16:34:17 +0500

    Added Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -32,4 +32,31 @@ public class CashbackHackServiceTest {
         int expected = 100;
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldRemainAmountIs0Jupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldRemain0AmountIsMultiple1000Jupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldRemainAmountNotMultiple1000Jupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
 }
