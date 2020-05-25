3949e9c5766cc8a6ed0cd2fbba229fa113946c54
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-25 11:36:48 +0500

    changed CashbackHackService

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,14 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount == 0) {
-            return boundary;
-        }
-        ;
-        if ((amount % boundary) == 0) {
-            return 0;
-        } else {
-            return boundary - amount % boundary;
-        }
+        return boundary - amount % boundary;
     }
 }
